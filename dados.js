9b8a20b3b2b20186ca0dd21e648bccb8afb9f6e2
gmalvesdev <galvesmeira@protonmail.com>
2024-09-07 14:43:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo: "O que é Apex Legends?",
+        descricao: "Apex Legends é um jogo de tiro em primeira pessoa gratuito, no estilo battle royale, que coloca jogadores em uma arena onde o objetivo é ser a última equipe de pé. O que o diferencia de outros jogos do gênero é a presença de Lendas, personagens com habilidades únicas que adicionam uma camada estratégica ao gameplay. Além do modo battle royale clássico, o jogo oferece diversos eventos e modos de tempo limitado, garantindo uma experiência sempre renovada. Desenvolvido pela Respawn Entertainment, o Apex Legends é conhecido por seu ritmo acelerado, jogabilidade intensa e um universo rico em lore e personagens carismáticos.",
+        link: "https://www.ea.com/pt-br/games/apex-legends/about",
+        tags: "jogo tiro battle royale gameplay gratuito",
+    },
+    {
+        titulo: "Sobre as Lendas do jogo",
+        descricao: "As Lendas de Apex Legends são personagens com habilidades únicas que trazem um toque estratégico ao jogo. Cada Lenda possui um conjunto de habilidades que podem ser usadas para auxiliar a equipe, como cura, escudos, habilidades de combate e muito mais. A escolha da Lenda certa pode fazer toda a diferença em uma partida.",
+        link: "https://www.ea.com/pt-br/games/apex-legends/about/characters",
+        tags: "habilidades personagens equipe",
+    },
+    {
+        titulo: "Sobre os Mapas do Jogo",
+        descricao: "Os mapas de Apex Legends são ambientes dinâmicos e em constante evolução, com diferentes paisagens e desafios. Cada mapa oferece oportunidades únicas para estratégia e combate. Explore os cantos mais remotos dos mapas e descubra os melhores locais para emboscar seus inimigos.",
+        link: "https://www.ea.com/pt-br/games/apex-legends/maps",
+        tags: "ambientes paisagens desafios estratégia locais",
+    },
+    {
+        titulo: "História de Apex Legends",
+        descricao: "Apex Legends se passa em um futuro distante nas Terras Ermas, um planeta devastado por guerras e catástrofes. Os Jogos Apex são um evento brutal onde as melhores Lendas do universo competem em arenas perigosas em busca de fama e fortuna. A história do jogo se desenrola através de trailers, quadrinhos e eventos in-game, revelando os mistérios por trás das Lendas e os motivos que as levam a participar dos Jogos. A narrativa é dinâmica e em constante evolução, com novas reviravoltas a cada temporada.",
+        link: "https://www.ea.com/pt-br/games/apex-legends/lore-hub",
+        tags: "Terras Ermas futuro Jogos jogos evento trailer quadrinhos mistérios",
+    }
+]
+
